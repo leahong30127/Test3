f78477e1532dde28824885a2befd0b39a0de277f
Ivan <cya011@163.com>
2025-02-12 14:20:38 +0800

    创建一个sub功能
    Signed-off-by: Ivan <cya011@163.com>

--- main.c
+++ main.c
@@ -3,6 +3,10 @@ int add(int a, int b)
 {
 	return a+b;
 }
+int sub(int a, int b)
+{
+	return a-b;
+}
 int main(int argc, char *argv[]) {
     printf("hello My world is wonderful!!\n");
     return 0;
