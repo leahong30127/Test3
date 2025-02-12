d84a4e38963251dcbf0a27771954f68279c21b56
Ivan <cya011@163.com>
2025-02-12 14:26:56 +0800

    增加 multyl功能
    Signed-off-by: Ivan <cya011@163.com>

--- main.c
+++ main.c
@@ -7,6 +7,10 @@ int sub(int a, int b)
 {
 	return a-b;
 }
+int multy(int a, int b)
+{
+	return a*b;
+}
 int main(int argc, char *argv[]) {
     printf("hello My world is wonderful!!\n");
     return 0;
